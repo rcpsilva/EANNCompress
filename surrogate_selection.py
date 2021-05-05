c6bdfdee129b5267d84eeebb92d76f0f8ffa7b8b
rcpsilva <rcpsilva@gmail.com>
2021-05-04 23:44:20 -0300

    changed surrogate selection api

--- surrogate_selection.py
+++ surrogate_selection.py
@@ -8,23 +8,8 @@ from sklearn.neighbors import KNeighborsRegressor
 def select_random(surrogate_ensemble,x,y):
     return random.choice(surrogate_ensemble)
 
-def select_by_MSE(surrogate_ensemble,x,y):
+def select_by_metric(surrogate_ensemble,metric,x,y):
     """ Select the best surrogate using the Mean Absolute Error
-    """
-    pass
-
-def select_by_MAE(surrogate_ensemble,x,y):
-    """ Select the best surrogate using the Mean Absolute Error
-    """
-    pass
-
-def select_by_MAPE(surrogate_ensemble,x,y):
-    """ Select the best surrogate using the Mean Absolute Percentual Error
-    """
-    pass
-
-def select_by_Spearman(surrogate_ensemble,x,y):
-    """ Select the best surrogate using Sepearman's rank correlation
     """
     pass
 
@@ -36,11 +21,13 @@ surrogate_ensemble = [DecisionTreeRegressor(),
 x = np.random.rand(100,3) * 10
 y = x[:,0]**2 + x[:,1]**2 + x[:,2]**2
 
-selected = select_by_MSE(surrogate_ensemble,x,y)
+metric = mean_squared_error
+
+selected = select_by_metric(surrogate_ensemble,metric,x,y)
 
 selected.fit(x,y)
 y_pred = selected.predict(x)
-accuracy = mean_squared_error(y, y_pred)
+accuracy = metric(y, y_pred)
 
 print(selected)
 print('Accuracy in the trainning set: {}'.format(accuracy))

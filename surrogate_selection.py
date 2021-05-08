7268c299a2d6cbebfa894fd6dcf645ceefb1eba3
Antonio de Barros <antonio.naddeomf@gmail.com>
2021-05-08 17:23:47 -0300

    bug-fix:adjusted selection with metric where less is better

--- surrogate_selection.py
+++ surrogate_selection.py
@@ -1,7 +1,7 @@
 import random
 import numpy as np
 from sklearn.metrics import mean_squared_error, make_scorer
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
@@ -19,7 +19,7 @@ def by_metric(surrogate_ensemble,metric,x,y, metric_great_is_better=True):
         model_scores = cross_val_score(model, x, y, scoring=metric)
         scores[index] = np.mean(model_scores)
     
-    index_best_model = np.argmax(scores)
+    index_best_model = np.argmax(scores) if metric_great_is_better else np.argmin(scores)
     return surrogate_ensemble[index_best_model]
 
 
@@ -37,13 +37,16 @@ if __name__ == "__main__":
     x = np.random.rand(100,3) * 10
     y = x[:,0]**2 + x[:,1]**2 + x[:,2]**2
 
+    X_train, X_test, y_train, y_test = train_test_split(x,y, test_size = 0.2)
     metric = mean_squared_error
 
-    selected = by_metric(surrogate_ensemble,metric,x,y, metric_great_is_better=False)
+    selected = by_metric(surrogate_ensemble,metric,X_train,y_train, metric_great_is_better=False)
+    
+    selected.fit(X_train, y_train)
+    y_pred = selected.predict(X_test)
+    accuracy = metric(y_test, y_pred)
 
-    selected.fit(x,y)
-    y_pred = selected.predict(x)
-    accuracy = metric(y, y_pred)
-
-print(selected)
-print('Accuracy in the trainning set: {}'.format(accuracy))
+    print(selected)
+    print('Accuracy in the trainning set: {}'.format(accuracy))
+    print(y)
+    print(y_pred)

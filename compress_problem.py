636d81f2806e234a9f3e50319fd38fe9a62fc034
Antonio de Barros <antonio.naddeomf@gmail.com>
2021-06-03 12:16:22 -0300

    add mask function for integer compression variables

--- compress_problem.py
+++ compress_problem.py
@@ -3,7 +3,6 @@ import pandas as pd
 from pymoo.util.misc import stack
 from pymoo.model.problem import Problem
 import tensorflow as tf
-import keras
 from keras.models import load_model
 import gc
 from keras import backend as K
@@ -16,20 +15,21 @@ class NNCompressProblem(Problem):
     def __init__(self, base_model, data, G=[]):
         self.base_model = base_model
         self.data = data
-        self.DF =  pd.DataFrame(columns=["Poda","Quantização","Camadas_Podadas","Esparsidade","Tipo_Quantização","Agenda_de_Poda","Frequencia_de_Poda","Acuracia","Tamanho"])
+        self.DF =  pd.DataFrame(columns=["Pruning","Quantization","Pruned_Layers","Sparsity","Quantization_type","Pruning_Schedule","Pruning_Frequency","Accuracy","Model_Size"])
         self.model_layers = nnUtils.layers_types(self.base_model)
         self.base_model_size = nnUtils.get_gzipped_model_file_size(base_model)
         self.num_type_layers = len(self.model_layers)
         num_unitary_problem_variable = 5
-        number_variables = num_unitary_problem_variable + self.num_type_layers +self.num_layers 
+        number_variables = num_unitary_problem_variable + self.num_type_layers * 2 # both sparsity and pruned layer have the same size that is the number of distinct layer in the model
         xl = np.zeros(number_variables) # bottom limit
         xu = np.ones(number_variables) # upper limit
         xl[-1] = 1 # bottom limit of pruning frequency
-        xu[-1] = 100 # upper limit of pruning frequency
+        xu[-1] = 200 # upper limit of pruning frequency
         self.G = G
+        n_constr = len(G)
         super().__init__(n_var=number_variables,
                          n_obj=2,
-                         n_constr=0,
+                         n_constr=n_constr,
                          xl=xl,
                          xu=xu,
                          elementwise_evaluation=True)
@@ -37,7 +37,8 @@ class NNCompressProblem(Problem):
 
     def _evaluate(self, x, out, *args, **kwargs):
         variable_vector = self.transform_variable_vector(X = x, num_type_layers = self.num_type_layers ,num_layers = self.num_layers)
-        base_model = self.base_model
+        base_model = tf.keras.models.clone_model(self.base_model)
+        base_model.set_weights(self.base_model.get_weights())
         acc,size = compress.eval_solution(X=variable_vector, model = base_model, model_layers= self.model_layers, data = self.data)
         K.clear_session()
         del base_model
@@ -46,7 +47,7 @@ class NNCompressProblem(Problem):
         out["F"] = [acc*-1, norm_size]
         out["G"] = self.G
 
-    def transform_variable_vector(X,num_type_layers):
+    def transform_variable_vector(self, X, num_type_layers):
         x1 = X[0] #executar poda
         x2 = X[1] #executar quantização 
         x3 = X[2:2+num_type_layers] # camadas a serem podadas
@@ -56,3 +57,13 @@ class NNCompressProblem(Problem):
         x7 = X[-1] #frequencia de poda
         vector=[x1,x2,x3,x4,x5,x6,x7]
         return vector
+
+    def get_compression_mask(self):
+        num_unitary_problem_variable = 5
+        num_type_layers = self.num_type_layers
+        number_variables = num_unitary_problem_variable + num_type_layers * 2
+        mask=["int"] * number_variables
+
+        for i in range(num_type_layers):
+            mask[2+num_type_layers+i]="real"
+        return mask

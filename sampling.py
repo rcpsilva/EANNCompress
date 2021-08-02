d5e44e4fb4842cffc1a39c47568f7c3f71a9703c
adriano-op <80333105+adriano-op@users.noreply.github.com>
2021-08-02 15:34:44 -0300

    alterado o arquivo sampling
    Alterado um parâmetro de comparação na função random_feasible.
    Incluído a função sampling e lhs

--- sampling.py
+++ sampling.py
@@ -3,6 +3,9 @@
 ######################################
 
 import numpy as np
+from pymoo.factory import get_sampling
+from pymoo.util import plotting
+from pymoo.interface import sample
 
 def rand(problem, n=100):
     """ Performs random sampling with an uniform distribution
@@ -49,11 +52,42 @@ def random_feasible(problem, n=100):
     while valid_samples <= n:
         sample = lb + np.random.rand(1,problem.n_var)*(ub-lb)
         F,G = problem.evaluate(sample[0])
-        if np.sum(G)==0:
-            samples['X'] = samples['X'] + sample
-            samples['F'] = samples['F'] + F
-            samples['G'] = samples['G'] + G
+        if np.sum(G)<=0 :  
+            samples['X'].append(sample)
+            samples['F'].append(F)
+            samples['G'].append(G)
 
             valid_samples += 1
 
+    return samples
+
+def sampling(problem, n=100):    
+    """ Performs random sampling with an uniform distribution
+    Args:
+        problem: a description of the problem for which the surrogate model 
+        is going to be built
+        n: Number of samples 
+
+    Returns:
+        samples: a set of n samples
+    """
+    lb = problem.xl 
+    ub = problem.xu      
+    x = lb + np.random.rand(n,problem.n_var)*(ub-lb)    
+    F = problem.evaluate(x) 
+    G = []
+    samples = { 'X': x,
+                'F': F,
+                'G': G}
+    return samples
+
+def lhs( n):
+    sampling = get_sampling('real_lhs' )
+    x = sample(sampling, n, 2)  
+    F = []
+    G = []
+    samples = { 'X': x,
+                'F': F,
+                'G': G}
+    plotting.plot(x, no_fill=True)
     return samples
